582e88c8ee3b039f44cbf12dd6a7e41ecaff57c5
Asad Khan <124061951+khanasad67@users.noreply.github.com>
2026-09-10 23:28:14 +0530

    Time: 0 ms (100%), Space: 23.8 MB (82.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& arr) {
-        int noz= 0;
-        int n=arr.size();
-        vector <int> v;
-        for(int i=0; i<n; i++){
-            if( arr[i]!=0) v.push_back(arr[i]);
-            else noz++;
+    //     int noz= 0;
+    //     int n=arr.size();
+    //     vector <int> v;
+    //     for(int i=0; i<n; i++){
+    //         if( arr[i]!=0) v.push_back(arr[i]);
+    //         else noz++;
+    //     }
+    //     for(int i=0; i<noz; i++){
+    //         v.push_back(0);
+    //     }
+    //     for(int i=0; i<n; i++){
+    //         arr[i]=v[i];
+    //     }
+    // }
+
+        int j=0;
+
+        for(int i=0; i<arr.size(); i++){
+            if(arr[i]!=0){
+                swap(arr[i],arr[j]);
+                j++;
+            }
         }
-        for(int i=0; i<noz; i++){
-            v.push_back(0);
-        }
-        for(int i=0; i<n; i++){
-            arr[i]=v[i];
-        }
-    }
+    }    
 };
+
