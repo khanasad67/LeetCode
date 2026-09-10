243525b4348fd21bcfcb41d24643c67934c9cf9d
Asad Khan <124061951+khanasad67@users.noreply.github.com>
2026-09-11 01:20:55 +0530

    Time: 0 ms (100%), Space: 20.5 MB (83.86%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int singleNumber(vector<int>& v) {
-        int n = v.size();
-        for(int i=0; i<n; i++){
-            bool flag = false;
-            for(int j=0; j<n ; j++){
-                if(j==i) continue;
-                if(v[i]==v[j]){
-                    flag=true;
-                    break;
-                }
-            }
-            if(flag == false) return v[i];
+        // int n = v.size();
+        // for(int i=0; i<n; i++){
+        //     bool flag = false;
+        //     for(int j=0; j<n ; j++){
+        //         if(j==i) continue;
+        //         if(v[i]==v[j]){
+        //             flag=true;
+        //             break;
+        //         }
+        //     }
+        //     if(flag == false) return v[i];
+        // }
+        // return 4;
+
+        int ans =0;
+        for(int i=0;i<v.size();i++){
+            ans = ans ^v[i];
         }
-        return 4;
+        return ans;
     }
 };
