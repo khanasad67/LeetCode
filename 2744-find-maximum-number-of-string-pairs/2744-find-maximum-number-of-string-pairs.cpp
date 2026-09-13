2140e56aa7e10addd4e92ae25a3a850556e107dc
Asad Khan <124061951+khanasad67@users.noreply.github.com>
2026-09-13 14:43:34 +0530

    Time: 0 ms (100%), Space: 25.7 MB (48.87%) - LeetHub

--- 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.cpp
+++ 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.cpp
@@ -5,15 +5,23 @@ public:
         int count=0;
         unordered_set<string>s;
 
+        // for(int i=0; i<n;i++){
+        //     s.insert(words[i]);
+        // }
+        // for(int i=0;i<n;i++){
+        //     string rev= words[i];
+        //     reverse(rev.begin(),rev.end());
+        //     if(rev==words[i]) continue;
+        //     if(s.find(rev)!=s.end()) count++;
+        //     s.erase(words[i]);
+        // }
+        // return count;
+
         for(int i=0; i<n;i++){
-            s.insert(words[i]);
-        }
-        for(int i=0;i<n;i++){
             string rev= words[i];
             reverse(rev.begin(),rev.end());
-            if(rev==words[i]) continue;
             if(s.find(rev)!=s.end()) count++;
-            s.erase(words[i]);
+            else s.insert(words[i]);
         }
         return count;
     }
