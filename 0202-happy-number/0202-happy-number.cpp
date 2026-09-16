2fe782e02c14e9371277fd2598b246b66bf446c6
Asad Khan <124061951+khanasad67@users.noreply.github.com>
2026-09-17 03:03:00 +0530

    Time: 0 ms (100%), Space: 8.3 MB (36.69%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+
+       unordered_set<int>st;
+
+       while(n!=1){
+        if(st.find(n)!=st.end()){
+            return false;
+        }
+        st.insert(n);
+
+        int sum=0;
+
+        while(n>0){
+            int digits =n%10;
+            sum +=digits*digits;
+            n=n/10;
+        }
+
+        n=sum;
+       }
+       return true;
+    }
+};
