48e53b5e24fbadc18c9d41ae4c60d96a58f1f920
Asad Khan <124061951+khanasad67@users.noreply.github.com>
2026-09-18 03:56:51 +0530

    Time: 35 ms (85.73%), Space: 46.9 MB (28.37%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,25 +1,45 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& nums, int k) {
+    int subarraySum(vector<int>& arr, int k) {
 
-        unordered_map<int, int> mp;
+        // unordered_map<int, int> mp;
 
-        mp[0] = 1;
+        // mp[0] = 1;
 
-        int sum = 0;
-        int ans = 0;
+        // int sum = 0;
+        // int ans = 0;
 
-        for(int i = 0; i < nums.size(); i++) {
+        // for(int i = 0; i < nums.size(); i++) {
 
-            sum += nums[i];
+        //     sum += nums[i];
 
-            if(mp.find(sum - k) != mp.end()) {
-                ans += mp[sum - k];
-            }
+        //     if(mp.find(sum - k) != mp.end()) {
+        //         ans += mp[sum - k];
+        //     }
 
-            mp[sum]++;
+        //     mp[sum]++;
+        // }
+
+        // return ans;
+
+        int n = arr.size();
+        vector<int> pre(n,0);
+        pre[0]=arr[0];
+        for(int i=1;i<n;i++){
+            pre[i]= arr[i]+pre[i-1];
         }
 
-        return ans;
+        unordered_map<int,int>mp;
+        int count =0;
+        for(int i=0; i<n;i++){
+            if(pre[i]==k){
+                count++;
+            }
+            int rem = pre[i]-k;
+            if(mp.find(rem)!=mp.end()) count+= mp[rem];
+            mp[pre[i]]++;
+
+        }
+        return count;
     }
 };
